d6fd1b3650cda5fcba80cad6dacc71b767c4f3fd
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-08-23 12:22:39 +0530

    Delete day3 directory

--- day3/src/main/java/com/example/demo/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
